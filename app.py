c448f25d945e1d3abbbcce4265fd3cb788c6bcab
pierrelouis-RECMA <pierrelouis@recma.com>
2026-04-22 12:10:15 +0200

    save

--- app.py
+++ app.py
@@ -5,6 +5,12 @@ from flask import Flask, abort, jsonify, render_template_string, request, send_f
 
 from generate_html import build_report_html
 
+@app.route("/report", methods=["POST"])  
+def report():
+    df = pd.read_excel(request.files["file"])
+    html = build_report_html(df)
+    return html, 200, {"Content-Type": "text/html; charset=utf-8"}
+
 app = Flask(__name__)
 app.config["MAX_CONTENT_LENGTH"] = 20 * 1024 * 1024
 
